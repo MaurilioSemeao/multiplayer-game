5a494f86600490b1c671da35f7dd487a76f0858c
maSemeao <maurilio.ssemeao@gmail.com>
2023-12-09 21:36:01 -0300

    update backend.js

--- backend.js
+++ backend.js
@@ -7,7 +7,8 @@ const server = http.createServer(app)
 const {Server} = require('socket.io')
 const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000})
 
-const port = 8080
+const port = 3000
+
 
 app.use(express.static('public'))
 
